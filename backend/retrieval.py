1bf467c530e1e34ad05c1de42d44390987356dee
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-06-20 16:59:24 +0100

    Added LLM implementation using Ollama(llama3.2:1b)

--- backend/retrieval.py
+++ backend/retrieval.py
@@ -1,3 +1,4 @@
+import ollama
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import CrossEncoder
@@ -13,22 +14,34 @@ db = Chroma(
     collection_metadata={"hnsw:space": "cosine"}
 )
 
-query = "Who is the Founder of Google?"
+query = input("Ask your Question: ")
 
-#Fetch more candidates
+#retrieve the docs
 base_retriever = db.as_retriever(search_kwargs={"k": 8})
 candidate_docs = base_retriever.invoke(query)
 
-#Rerank using CrossEncoder directly
+#Reranking the candidate docs
 reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 pairs = [[query, doc.page_content] for doc in candidate_docs]
 scores = reranker.predict(pairs)
 
-#Sort by score and take top 3
 ranked = sorted(zip(scores, candidate_docs), key=lambda x: x[0], reverse=True)
 top_docs = [doc for _, doc in ranked[:3]]
 
-print(f"Query: {query}")
-print("Context:")
-for i, (score, doc) in enumerate(ranked[:3], 1):
-    print(f"Answer {i}:\n{doc.page_content}\n")
+#LLM implementatiom
+def generate_answer(query, docs):
+    context = "\n\n".join([f"[Source {i+1}]: {doc.page_content}" for i, doc in enumerate(docs)])
+    prompt = f"""Answer using ONLY the context below.
+    If the answer isn't there, say "I don't have enough information."
+    Context:
+    {context}
+    Question: {query}
+    Answer:"""
+    response = ollama.chat(
+        model="llama3.2:1b",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.message.content
+
+answer = generate_answer(query, top_docs)
+print(f"Response: {answer}")
