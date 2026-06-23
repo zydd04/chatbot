a19476b8c3fce5e79063026a93599da5a33d9721
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-06-23 16:10:55 +0100

    UI Implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState, useEffect, useRef  } from "react";
+import { useState, useEffect, useRef } from "react";
 const API = "http://127.0.0.1:8000";
 
 export default function App() {
-    const [files, setFiles] = useState([]);
-    const [message, setMessage] = useState([]);
-    const [history, setHistory] = useState([]);
-    const bottomRef = useRef(null);
-    const [loading, setLoading] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [message, setMessage] = useState([]);
+  const [history, setHistory] = useState([]);
+  const bottomRef = useRef(null);
+  const textareaRef = useRef(null); // Added definition to prevent crash
+  const [loading, setLoading] = useState(false);
 
-    //File Handling
-    const loadFiles = async () => {
-        try {
-            const res = await(fetch(`${API}/docs`))
-            const data = await res.json();
-            setFiles(data.files || []);
-        } catch (err) {
-        console.error("File load error:", err);
-        }
-    };
-    useEffect(() => {
-        loadFiles();
-    }, []);
+  //File Handling
+  const loadFiles = async () => {
+    try {
+      const res = await fetch(`${API}/docs`);
+      const data = await res.json();
+      setFiles(data.files || []);
+    } catch (err) {
+      console.error("File load error:", err);
+    }
+  };
 
-    useEffect(() => {
-        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [history]);
+  useEffect(() => {
+    loadFiles();
+  }, []);
 
-    //Upload handling
-    const handleUpload = async(e) => {
-        const file = e.target.docs[0];
-        if (!file) return; //empty
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [history]);
+
+  //Upload handling
+  const handleUpload = async (e) => {
+    const file = e.target.docs[0];
+    if (!file) return; //empty
     const formData = new FormData();
     formData.append("file", file);
     await fetch(`${API}/upload`, {
@@ -37,100 +39,100 @@ export default function App() {
       body: formData,
     });
     loadFiles();
-        e.target.value = "";
+    e.target.value = "";
+  };
+
+  //Delete handling
+  const deleteFile = async (fname) => {
+    await fetch(`${API}/docs/${filename}`, {
+      method: "DELETE",
+    });
+    loadFiles(); //reload
+  };
+
+  //Send handling
+  const sendMessage = async () => {
+    if (!message.trim() || loading) return; //can't send while loading or empty message
+    const userMsg = {
+      role: "user",
+      content: message.trim(),
     };
+    const updatedHistory = [...history, userMsg]; //update history
+    setHistory(updatedHistory);
+    setMessage("");
+    setLoading(true);
+    setHistory((prev) => [
+      ...prev,
+      {
+        role: "assistant",
+        content: "",
+        streaming: true,
+        sources: [],
+      },
+    ]);
+    try {
+      const res = await fetch(`${API}/chat`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: userMsg.content,
+          history: updatedHistory,
+        }),
+      });
+      //read
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+      let buffer = "";
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
 
-    //Delete handling
-    const deleteFile = async (fname) => {
-        await fetch(`${API}/docs/${filename}`, {
-        method: "DELETE",
-        });
-        loadFiles(); //reload
-    };
+        buffer += decoder.decode(value, { stream: true });
+        const lines = buffer.split("\n");
+        buffer = lines.pop();
 
-    //Send handling
-    const sendMessage = async () => {
-        if (!message.trim() || loading) return; //can't send while loading or empty message
-        const userMsg = {
-        role: "user",
-        content: message.trim(),
-        };
-        const updatedHistory = [...history, userMsg]; //update history
-        setHistory(updatedHistory);
-        setMessage("");
-        setLoading(true);
-        setHistory((prev) => [
-            ...prev,
-            {
-            role: "assistant",
-            content: "",
-            streaming: true,
-            sources: [],
-            },
-        ]);
-        try {
-            const res = await fetch(`${API}/chat`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    message: userMsg.content,
-                    history: updatedHistory,
-                }),
-            });
-            //read
-            const reader = res.body.getReader();
-            const decoder = new TextDecoder();
-            let buffer = "";
-            while (true) {
-                const { done, value } = await reader.read();
-                if (done) break;
+        for (const line of lines) {
+          if (!line.trim()) continue;
 
-                buffer += decoder.decode(value, { stream: true });
-                const lines = buffer.split("\n");
-                buffer = lines.pop();
+          let data;
+          try {
+            data = JSON.parse(line);
+          } catch {
+            continue;
+          }
 
-                for (const line of lines) {
-                    if (!line.trim()) continue;
+          //show chunks
+          if (data.type === "chunk") {
+            setHistory((prev) => {
+              const updated = [...prev];
+              const last = updated[updated.length - 1];
 
-                let data;
-                try {
-                    data = JSON.parse(line);
-                } catch {
-                    continue;
-                }
-                
-                //show chunks
-                if (data.type === "chunk") {
-                    setHistory((prev) => {
-                        const updated = [...prev];
-                        const last = updated[updated.length - 1];
-
-                        if (last?.role === "assistant") {
-                            updated[updated.length - 1] = {
-                                ...last,
-                                content: last.content + data.text,
-                            };
-                        }
-
-                    return updated;
-                    });
-                }
-            }
-        }
-        } catch (error) {
-        setHistory((prev) => {
-            const updated = [...prev];
-            const last = updated[updated.length - 1];
-
-            if (last?.role === "assistant") {
+              if (last?.role === "assistant") {
                 updated[updated.length - 1] = {
-                role: "assistant",
-                content: "Error: " + error.message,
-                streaming: false,
-                sources: [],
-            };
+                  ...last,
+                  content: last.content + data.text,
+                };
+              }
+
+              return updated;
+            });
+          }
+        }
+      }
+    } catch (error) {
+      setHistory((prev) => {
+        const updated = [...prev];
+        const last = updated[updated.length - 1];
+
+        if (last?.role === "assistant") {
+          updated[updated.length - 1] = {
+            role: "assistant",
+            content: "Error: " + error.message,
+            streaming: false,
+            sources: [],
+          };
         }
         return updated;
       });
@@ -139,6 +141,7 @@ export default function App() {
     setLoading(false);
     textareaRef.current?.focus();
   };
+
   //shift+enter handler for new line
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
@@ -147,5 +150,63 @@ export default function App() {
     }
   };
 
-    
-} 
+  //UI
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <h2>RAG Chat</h2>
+
+        <label className="upload">
+          Upload File
+          <input type="file" onChange={handleUpload} hidden />
+        </label>
+
+        <div className="files">
+          {files.map((f, i) => (
+            <div key={i} className="file">
+              <span>{f}</span>
+              <button onClick={() => removeFile(f)}>X</button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="chat">
+        <div className="messages">
+          {history.map((msg, i) => (
+            <div key={i} className={`msg ${msg.role}`}>
+              <div className="bubble">
+                <pre>{msg.content}</pre>
+                {msg.streaming && <span className="cursor">▍</span>}
+              </div>
+
+              {msg.sources?.length > 0 && (
+                <div className="sources">
+                  <h4>Sources</h4>
+                  {msg.sources.map((s, idx) => (
+                    <div key={idx} className="source">
+                      <strong>{s.file}</strong>
+                      <p>{s.preview}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+
+          <div ref={bottomRef} />
+        </div>
+
+        <div className="input">
+          <textarea
+            ref={textareaRef}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Ask something..."
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
